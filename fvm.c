2e439a6467ea4e4420aa2cc55d0960638fae792f
phase <ph4sesaber@gmail.com>
2015-05-11 21:09:51 -0700

    Rename `reg#` to `n#`
    In case I use them for something other than registers!

--- fvm.c
+++ fvm.c
@@ -9,16 +9,16 @@ int pc = 0;
 
 /* Fields */
 int instruction = 0;
-int reg1 = 0;
-int reg2 = 0;
-int reg3 = 0;
+int n1 = 0;
+int n2 = 0;
+int n3 = 0;
 int imm = 0;
 
 void decode(int in){
   instruction = (in & 0xFF000) >> 16;
-  reg1 = (in & 0xF00) >>  8;
-  reg2 = (in & 0xF0) >>  4;
-  reg3 = (in & 0xF);
+  n1 = (in & 0xF00) >>  8;
+  n2 = (in & 0xF0) >>  4;
+  n3 = (in & 0xF);
   imm = (in & 0xFF);
 }
 
@@ -31,25 +31,25 @@ void eval(){
       running = 0;
       break;
     case 1: /*ldi*/
-      regs[reg1] = imm;
+      regs[n1] = imm;
       break;
     case 2: /*rst*/
-      regs[reg1] = 0;
+      regs[n1] = 0;
       break;
     case 3: /*gto*/
       pc = imm;
       break;
     case 4: /*add*/
-      regs[reg3] = regs[reg1] + regs[reg2];
+      regs[n3] = regs[n1] + regs[n2];
       break;
     case 5: /*sub*/
-      regs[reg3] = regs[reg1] - regs[reg2];
+      regs[n3] = regs[n1] - regs[n2];
       break;
     case 6: /*mul*/
-      regs[reg3] = regs[reg1] * regs[reg2];
+      regs[n3] = regs[n1] * regs[n2];
       break;
     case 7: /*div*/
-      regs[reg3] = regs[reg1] / regs[reg2];
+      regs[n3] = regs[n1] / regs[n2];
       break;
     default:
       printf("Error: Instruction %n not found!", instruction);
