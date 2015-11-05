753b79c9b4cca6438b18444ffaf44da03288bc9a
phase <ph4sesaber@gmail.com>
2015-11-04 18:18:17 -0800

    Fix printing

--- src/fvm/instructions.d
+++ src/fvm/instructions.d
@@ -5,24 +5,23 @@ import fvm.std;
 void readInstructions(ubyte[] ins) {
     for(int i = 0; i < ins.length; i++) {
         ubyte j = ins[i];
-        writeln("ubyte: " ~ to!string(j));
+        //writeln("instruction: " ~ to!string(j));
         //commands
         if(j == 0) { ///load <reg> <#>
             if(ins.length < i + 2) ///if the program doesn't have enough arguments for the instruction
-                throw new Exception("Malformed bytecode @ " ~ to!string(i) ~ "/" ~ to!string(ins.length));
-            ubyte register = ins[i+1];
-            long value = cast(long) ins[i+2];
+                throw new Exception("LoadError: Malformed bytecode @ " ~ to!string(i) ~ "/" ~ to!string(ins.length));
+            ubyte register = ins[++i];
+            long value = cast(long) ins[++i];
             new Register(register, value);
-            i += 2;
         }
-        else if(j == 10) {
+        else if(j == 10) { ///print <reg>
             if(ins.length < i + 1)
-                throw new Exception("Malformed bytecode @ " ~ to!string(i));
-            ubyte register = ins[i+1];
-            write(Register.getValue(register));
+                throw new Exception("PrintError: Malformed bytecode @ " ~ to!string(i));
+            ubyte register = ins[++i];
+            write(cast(char)Register.getValue(register));
         }
         else {
-            throw new Exception("Malformed bytecode @ " ~ to!string(i));
+            throw new Exception("ParseError: Malformed bytecode @ " ~ to!string(i));
         }
     }
 }
