5339c505a0fcad90ed74ddcbf485a867f3f9c36d
phase <ph4sesaber@gmail.com>
2015-11-04 19:01:41 -0800

    Support arrays in registers

--- src/fvm/instructions.d
+++ src/fvm/instructions.d
@@ -7,18 +7,30 @@ void readInstructions(ubyte[] ins) {
         ubyte j = ins[i];
         //writeln("instruction: " ~ to!string(j));
         //commands
-        if(j == 0) { ///load <reg> <#>
+        if(j == 0) { ///loadi <reg> <#>
             if(ins.length < i + 2) ///if the program doesn't have enough arguments for the instruction
                 throw new Exception("LoadError: Malformed bytecode @ " ~ to!string(i) ~ "/" ~ to!string(ins.length));
             ubyte register = ins[++i];
             long value = cast(long) ins[++i];
             new Register(register, value);
         }
+        else if(j == 1) { ///loada <reg> <#...
+            if(ins.length < i + 2)
+                throw new Exception("LoadError: Malformed bytecode @ " ~ to!string(i) ~ "/" ~ to!string(ins.length));
+            ubyte register = ins[++i];
+        }
         else if(j == 10) { ///print <reg>
             if(ins.length < i + 1)
                 throw new Exception("PrintError: Malformed bytecode @ " ~ to!string(i));
             ubyte register = ins[++i];
-            write(cast(char)Register.getValue(register));
+            Register reg = Register.getRegister(register);
+            switch(reg.getType()) {
+                case Register.Type.NUMBER:
+                    write(cast(char)reg.getValue());
+                case Register.Type.ARRAY:
+                    write(cast(char[])reg.getValues());
+                default: break;
+            }
         }
         else {
             throw new Exception("ParseError: Malformed bytecode @ " ~ to!string(i));
--- src/fvm/register.d
+++ src/fvm/register.d
@@ -1,19 +1,29 @@
 module fvm.register;
 import fvm.std;
+import std.algorithm;
 
 class Register {
+    enum Type : string {
+        NUMBER = "Number", ARRAY = "Array"
+    }
+    
     static Register[] registers;
     ubyte id;
+    Type type;
     long value = 0;
+    int[] values;
     
-    this(ubyte id) {
+    this(ubyte id, int[] values) {
         this.id = id;
+        this.values = values;
+        this.type = Type.ARRAY;
         registers ~= this;
     }
     
     this(ubyte id, long value) {
         this.id = id;
         this.value = value;
+        this.type = Type.NUMBER;
         registers ~= this;
     }
     
@@ -21,22 +31,38 @@ class Register {
         return id;
     }
     
+    Type getType() {
+        return type;
+    }
+    
     long getValue() {
+        if(type != Type.NUMBER)
+            throw new Exception("RegisterTypeError: Tried to get number value from a register with type " ~ type);
         return value;
     }
     
+    int[] getValues() {
+        if(type != Type.ARRAY)
+            throw new Exception("RegisterTypeError: Tried to get array value from a register with type " ~ type);
+        return values;
+    }
+    
     override string toString() {
-        return "Register[" ~ to!string(id) ~ "]: " ~ to!string(value);
+        return type ~ "Register[" ~ to!string(id) ~ "]";
     }
     
     static Register getRegister(ubyte id) {
         foreach(register; registers)
             if(register.getId() == id)
                 return register;
-        return new Register(id);
+        return new Register(id, 0);
     }
     
-    static long getValue(ubyte id) {
-        return getRegister(id).getValue();
+    static void deallocateRegister(ubyte id) {
+        Register reg = getRegister(id);
+        if(reg is null)
+            throw new Exception("DeallocRegisterError: Register with id " ~ to!string(id) ~ " does not exist");
+        auto index = countUntil(registers, reg);
+        registers = remove(registers, index);
     }
 }
